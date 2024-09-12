f864b8aa891f97c941803607025d56775fa8b7fa
Nguyen The Minh <themingw666@gmail.com>
2024-09-12 15:32:56 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       Hello world
       <div>I' Grey</div>
-      <div>Thanks you Grey, I'm Xiao Ming.. testing your app</div>
+      <div>Thanks you Grey, I'm Xiao Ming.. testing your app 666666</div>
       <div className="App" style={containerStyle}>
       <img src="https://top10az.com/wp-content/uploads/2021/06/top-10-hacker-noi-tieng-nhat-viet-nam.jpg" alt="Mô tả hình ảnh" style={imageStyle} />
     </div>
